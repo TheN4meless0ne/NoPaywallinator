b0fabd21eb423cdddfc52ae7d99a8bd61bd056c0
TheN4meless0ne <thenmelessne@gmail.com>
2024-12-06 09:55:22 +0100

    added support for digi.no
    removed skroll lock
    removed overlay
    removed background blur

--- content.js
+++ content.js
@@ -10,6 +10,16 @@ function removeOverlay() {
     }
 }
 
+function removeOverlay2() {
+    'use strict';
+
+    var elems = document.querySelectorAll('.fixed.z-\\[999\\].inset-0.flex.items-center.overflow-y-scroll');
+    elems.forEach(function(elem) {
+        elem.classList.remove('fixed', 'inset-0', 'w-full', 'h-full', 'bg-[#121214]', 'bg-opacity-90', 'z-[99]');
+    });
+    console.log('Overlay 2 removed');
+}
+
 function removeBackgroundBlur() {
     'use strict';
 
@@ -20,16 +30,43 @@ function removeBackgroundBlur() {
     console.log('Background blur class removed');
 }
 
+function removeBackgroundBlur2() {
+    'use strict';
+
+    var elems = document.querySelectorAll('.fixed.inset-0.w-full.h-full.bg-\\[\\#121214\\].bg-opacity-90.z-\\[99\\]');
+    elems.forEach(function(elem) {
+        elem.classList.remove('fixed', 'inset-0', 'w-full', 'h-full', 'bg-[#121214]', 'bg-opacity-90', 'z-[99]');
+    });
+    console.log('Background blur 2 class removed');
+}
+
+function removeScrollLock() {
+    'use strict';
+
+    var elems = document.querySelectorAll('[class*="overflow-hidden"][class*="h-screen"]');
+    elems.forEach(function(elem) {
+        elem.classList.remove('overflow-hidden', 'h-screen');
+    });
+    console.log('Scroll lock removed');
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     removeOverlay();
+    removeOverlay2();
     removeBackgroundBlur();
+    removeBackgroundBlur2();
+    removeScrollLock();
 });
 
 // Use MutationObserver to handle dynamically added elements
 const observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         removeOverlay();
+        removeOverlay2();
         removeBackgroundBlur();
+        removeBackgroundBlur2();
+        removeScrollLock();
     });
 });
 
